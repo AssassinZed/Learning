4de6f3accd0fbae8a29a9a82e995296d49a11417
AssassinZed <1366565586@qq.com>
2018-08-19 09:22:49 +0800

    many files

---
+++ Vue/demo/src/components/layout.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <div class="app-header">header</div>
+    <div class="app-content">
+        <keep-alive>
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+    <div class="app-footer">footer</div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    p {
+        font-size: 16px;
+    }
+</style>
+
